1dc324c3d83feb5300cbe4ab8c5bd71c97107e25
tehpsalmist <bstewardcodes@gmail.com>
2019-12-03 12:48:47 -0500

    refactor: list promise return value first

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ rpcClient.start()
 /**
  * Get Context Data from Zengine Admin state
  */
-export function znContext (callback: (err: Error, context: ZengineContextData) => void): null
 export function znContext (): Promise<ZengineContextData>
+export function znContext (callback: (err: Error, context: ZengineContextData) => void): null
 
 export function znContext (callback?: (err: Error, context: ZengineContextData) => void): Promise<ZengineContextData> | null {
   return rpcClient.call({ method: 'context', callback })
@@ -22,8 +22,8 @@ export function znContext (callback?: (err: Error, context: ZengineContextData) 
  * Displays a confirmation dialog with your message and two buttons: Yes and Close
  * Sends a boolean to your promise or callback representing the user's selection
  */
-export function znConfirm (message: string, callback: (err: Error, confirmed: boolean) => void): null
 export function znConfirm (message: string): Promise<boolean>
+export function znConfirm (message: string, callback: (err: Error, confirmed: boolean) => void): null
 
 export function znConfirm (message: string, callback?: (err: Error, confirmed: boolean) => void): Promise<boolean> | null {
   return rpcClient.call({ method: 'confirm', args: { message }, callback })
@@ -42,8 +42,8 @@ export function znMessage (message: string, type: 'info' | 'saved' | 'warning' |
 /**
  * Displays a modal that allows the user to view and build a data filter.
  */
-export function znFiltersPanel (options: ZengineFiltersPanelOptions, callback: (err: Error, filter: ZengineFilter) => void): null
 export function znFiltersPanel (options: ZengineFiltersPanelOptions): Promise<ZengineFilter>
+export function znFiltersPanel (options: ZengineFiltersPanelOptions, callback: (err: Error, filter: ZengineFilter) => void): null
 
 export function znFiltersPanel (options: ZengineFiltersPanelOptions, callback?: (err: Error, filter: ZengineFilter) => void): Promise<ZengineFilter> | null {
   return callback
@@ -90,8 +90,8 @@ export function stopAutoSizing () {
 /**
  * Make a call to the Zengine API
  */
-export function znHttp (request: ZengineAPIRequestOptions, callback: (err: Error, resp: ZengineHTTPResponse) => void): null
 export function znHttp (request: ZengineAPIRequestOptions): Promise<ZengineHTTPResponse>
+export function znHttp (request: ZengineAPIRequestOptions, callback: (err: Error, resp: ZengineHTTPResponse) => void): null
 
 export function znHttp (request: ZengineAPIRequestOptions, callback?: (err: Error, resp: ZengineHTTPResponse) => void): Promise<ZengineHTTPResponse> | null {
   return rpcClient.call({
